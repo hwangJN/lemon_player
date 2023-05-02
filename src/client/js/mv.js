076b243ff4bfd9b480e8d9c1e469d2192f25d897
Hwang Jina <wlsk401@gmail.com>
2023-05-02 22:47:33 +0900

    mv modal 스크롤 방지

--- src/client/js/mv.js
+++ src/client/js/mv.js
@@ -5,14 +5,15 @@ if (window.location.pathname === "/") {
   const playMv = document.querySelector("#play-video");
   const close = playMv.querySelector(".close");
   const iframe = playMv.querySelector("iframe");
+  const body = document.querySelector("body");
 
   eachMv.forEach((mv) => {
     mv.addEventListener("click", () => {
       pauseMusic();
+      body.classList.add("stop-scrolling");
       const youtubeID = mv.getAttribute("id");
       iframe.src = `https://www.youtube-nocookie.com/embed/${youtubeID}?enablejsapi=1&version=3&playerapiid=ytplayer `;
       playMv.style.display = "block";
-
       // 스크롤 위치
       const scrollTop =
         window.pageYOffset || document.documentElement.scrollTop;
@@ -29,6 +30,7 @@ if (window.location.pathname === "/") {
           "*"
         );
         playMv.style.display = "none";
+        body.classList.remove("stop-scrolling");
       });
     });
   });
